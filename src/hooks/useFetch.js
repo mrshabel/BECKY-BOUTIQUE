72ab8b3b67a6b1ccb8f19114e3effb7de6ffd620
mrshabel <sgumah001@st.ug.edu.gh>
2023-08-13 15:05:11 +0000

    fix

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,29 +1,25 @@
 import { useEffect, useState } from "react";
-export default function useFetch(link) {
-  const [data, setData] = useState([]);
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState(null);
   console.log(data, loading, error);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const result = await fetch(link);
+        const result = await fetch(url);
         const fetchedData = await result.json();
-
         setData(fetchedData);
-        setLoading(false);
-
-        setError("");
       } catch (error) {
-        setLoading(false);
-        setData("");
         setError(error);
+      } finally {
+        setLoading(false);
       }
     };
     return () => fetchData();
-  }, []);
+  }, [url]);
 
   return { data, loading, error };
 }
