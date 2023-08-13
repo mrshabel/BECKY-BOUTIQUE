86cf0a3f0b945050e424a0fe15c1aafa9852ac0a
mrshabel <sgumah001@st.ug.edu.gh>
2023-08-13 14:27:33 +0000

    fix: api data fetching

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
 export default function useFetch(link) {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   console.log(data, loading, error);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const result = await fetch(link);
         const fetchedData = await result.json();
 
-        setLoading(false);
         setData(fetchedData);
+        setLoading(false);
+
         setError("");
       } catch (error) {
         setLoading(false);
